ea6236908eb098fe35b9f6e5a6fd64754c9f9f84
Marina Vabistsevits <ny19205@bristol.ac.uk>
2020-05-17 22:57:16 +0100

    parse second skills sheet

--- gather_data.R
+++ gather_data.R
@@ -20,7 +20,8 @@ if(using_googlesheets){
   
   
   position_data <- read_sheet(positions_sheet_loc, sheet = "positions")
-  skills        <- read_sheet(positions_sheet_loc, sheet = "language_skills")
+  tech_skills        <- read_sheet(positions_sheet_loc, sheet = "tech_skills")
+  other_skills        <- read_sheet(positions_sheet_loc, sheet = "other_skills")
   text_blocks   <- read_sheet(positions_sheet_loc, sheet = "text_blocks")
   contact_info  <- read_sheet(positions_sheet_loc, sheet = "contact_info", skip = 1)
   
